c6e31a40786c8cbb338a4be2e53efb1f3708d812
retropldn <retropldn@gmail.com>
2026-02-03 16:06:14 +0400

    two_pointers practice, 4sum and retry two_sum_array_sorted

--- medium/two_pointers/167_Two_Sum_II_Input_Array_Is_Sorted.py
+++ medium/two_pointers/167_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -12,15 +12,14 @@ O(1) additional space.
 '''
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        l,r = 0, len(numbers)-1
-
-        while l < r:
-            if numbers[l] + numbers[r] == target:
-                return [l+1,r+1]
-            elif numbers[l] + numbers[r] > target:
-                r -= 1
-            elif numbers[l] + numbers[r] < target:
-                l += 1
+        left, right = 0, len(numbers)-1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right+1]
+            elif numbers[left] + numbers[right] > target:
+                right -=1
+            elif numbers[left] + numbers[right] < target:
+                left -= 1
 
 assert Solution().twoSum(numbers = [1,2,3,4], target = 3) == [1,2]
         
--- medium/two_pointers/18_4Sum.py
+++ medium/two_pointers/18_4Sum.py
@@ -10,7 +10,30 @@ You may return the answer in any order.
 
 class Solution:
     def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
-        ...
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue;
+            for j in range(i+1,len(nums)):
+                if j > i+1 and  nums[j] == nums[j-1]:
+                    continue;
+                left = j+1
+                right = len(nums)-1
+                while left < right:
+                    if nums[i] + nums[j] + nums[left] + nums[right] == target:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right+1]:
+                            right -= 1
+                    elif nums[i] + nums[j] + nums[left] + nums[right] < target:
+                        left += 1
+                    elif nums[i] + nums[j] + nums[left] + nums[right] > target:
+                        right -= 1
+        return res
 
 assert Solution().fourSum(nums = [1,0,-1,0,-2,2], target = 0) == [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 assert Solution().fourSum(nums = [2,2,2,2,2], target = 8) == [[2,2,2,2]]
