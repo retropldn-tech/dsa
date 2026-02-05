3655f1b81d81d05917a3aafd3c72bcdb0375813b
retropldn <retropldn@gmail.com>
2026-02-05 16:53:12 +0400

    middle of linked list

---
+++ easy/two_pointers_fixed_separation/876_Middle_of_the_Linked_List.py
@@ -0,0 +1,20 @@
+'''
+    Given the head of a singly linked list, return the middle node of the linked list.
+    If there are two middle nodes, return the second middle node.
+'''
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: ListNode | None) -> ListNode | None:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
